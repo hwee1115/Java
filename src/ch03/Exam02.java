c06b5182bd7f4557448d93ae3afe58816fcd0f3e
hwee1115 <hwee1115@naver.com>
2021-02-16 23:24:10 +0900

    review

--- src/ch03/Exam02.java
+++ src/ch03/Exam02.java
@@ -8,15 +8,17 @@ public class Exam02 {
 		int var1 = 2;
 		int var2 = 3;
 		
-		boolean var3 = var1 == var2;
-		boolean var4 = var1 != var2;
-		boolean var5 = var1 > var2;
-		boolean var6 = var1 >= var2;
-		boolean var7 = var1 < var2;
-		boolean var8 = var1 <= var2;
-		boolean var9 = !(var1 == var2);
-		boolean var10 = (var1 == var2) || (var1< var2);
-		boolean var11 = (var1 == var2) && (var1< var2);
+		boolean var3 = var1 == var2;	//false
+		boolean var4 = var1 != var2;	//true
+		boolean var5 = var1 > var2;		//false
+		boolean var6 = var1 >= var2;	//false
+		boolean var7 = var1 < var2;		//true
+		boolean var8 = var1 <= var2;	//true
+		boolean var9 = !(var1 == var2);	//true
+		boolean var10 = (var1 == var2) || (var1< var2);	//true
+		boolean var11 = (var1 == var2) && (var1< var2);	//false
+		
+		System.out.println(var10);
 	}
 	
 
